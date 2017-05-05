acedc409f9a808087c923ebbc2e294ef92789eec
Marie Gadda <mariegadda@gmail.com>
2017-05-05 09:01:38 -0400

    working basic card game

---
+++ scripts/generator.js
@@ -0,0 +1,25 @@
+
+var input = process.argv[3];
+var inquirer = require("inquirer");
+
+function startGame(){
+  inquirer.prompt([
+      {
+        type: "list",
+        message: "Welcome to Trivia! Would you like to play Friends Basic Flashcard or Seinfeld Cloze Flashcard? \n ",
+          name: "ForS",
+        choices: ["Friends", "Seinfeld"]
+      
+      }
+    ]).then(function(answers){
+        if (answers.ForS === "Friends") {
+          console.log("Guess which Friends character said it!");
+          var BasicCard = require('./BasicCard');
+         BasicCard;
+        }else{
+          console.log("Seinfeld trivia coming soon!");
+        }
+    });
+}
+
+startGame();
