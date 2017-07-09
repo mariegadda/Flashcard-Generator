35a67b047a87568263e8c4efe4279b1a559c1e4f
Marie Gadda <mariegadda@gmail.com>
2017-07-08 21:28:52 -0400

    updating readme

--- scripts/ClozeCard.js
+++ scripts/ClozeCard.js
@@ -13,35 +13,27 @@ function ClozeCard(text, cloze){
 //array of clozecard data
 var clozeCardArray = [
 	{
-		text: "I don’t think I’ve ever been to an appointment in my life where I wanted the other guy to show up"
+		text: "I don’t think I’ve ever been to an appointment in my life where I wanted the other guy to show up",
 		cloze: "George"
 	},
 	{
-		text: "You know what would make a great coffee table book? A coffee table book about coffee tables"
+		text: "You know what would make a great coffee table book? A coffee table book about coffee tables",
 		cloze: "Kramer"
 	},
 	{
-		text: "There’s more to life than making shallow, fairly obvious observations"
+		text: "There’s more to life than making shallow, fairly obvious observations",
 		cloze: "Jerry"
 	},
 	{
-		text: "My life is the complete opposite of everything I want it to be. Every instinct I have, in every aspect of life, be it something to wear, something to eat – it’s all been wrong"
+		text: "My life is the complete opposite of everything I want it to be. Every instinct I have, in every aspect of life, be it something to wear, something to eat – it’s all been wrong",
 		cloze: "George"
 	},
 	{
-		text: "Do you have any idea how much time I waste in this apartment?"
+		text: "Do you have any idea how much time I waste in this apartment?",
 		cloze: "Kramer"
 	}
 	];
 
 
-module.exports = ClozecCard;
+module.exports = ClozeCard;
 
-
-// This file should define a Node module that exports a constructor for creating cloze-deletion flashcards, e.g.: module.exports = ClozeCard;
-// The constructor should accept two arguments: text and cloze.
-// The constructed object should have a cloze property that contains only the cloze-deleted portion of the text.
-// The constructed object should have a partial property that contains only the partial text.
-// The constructed object should have a fullText property that contains only the full text.
-// The constructor should throw or log an error when the cloze deletion does not appear in the input text.
-// Use prototypes to attach these methods, wherever possible.
--- scripts/generator.js
+++ scripts/generator.js
@@ -1,24 +1,22 @@
-
 var input = process.argv[3];
 var inquirer = require("inquirer");
 
 //startgame functions asks if user wants to play friends trivia or seinfeld - seinfeld not working yet. 
-function startGame(){
-  inquirer.prompt([
-      {
+function startGame() {
+    inquirer.prompt([{
         type: "list",
         message: "Welcome to Trivia! Would you like to play Friends Basic Flashcard or Seinfeld Cloze Flashcard? \n ",
-          name: "ForS",
+        name: "ForS",
         choices: ["Friends", "Seinfeld"]
-      
-      }
-    ]).then(function(answers){
+
+    }]).then(function(answers) {
         if (answers.ForS === "Friends") {
-          console.log("Guess which Friends character said it!");
-          var BasicCard = require('./BasicCard');
-         BasicCard;
-        }else{
-          console.log("Seinfeld trivia coming soon!");
+            console.log("Guess which Friends character said it!");
+            var BasicCard = require('./BasicCard');
+            BasicCard;
+        } else {
+            console.log("Seinfeld trivia coming soon!");
+
         }
     });
 }
